c8cec90657e8005daedc6d2f693de948eddcf263
Surich Caso <surichcv@gmail.com>
2025-06-13 14:19:31 -0500

    Estructura base del proyecto generada con Spring Initializr

---
+++ src/main/java/com/yautalk/YauTalkApplication.java
@@ -0,0 +1,13 @@
+package com.yautalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YauTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YauTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yautalk/YauTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yautalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YauTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
